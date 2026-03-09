ae0146225bdcd6b112b5d4557ae61b6bbc4e2cfe
Mike Panitz <mike.panitz@gmail.com>
2026-03-09 01:43:32 -0700

    added logging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,14 @@
  * For more details on building Java & JVM projects, please refer to https://docs.gradle.org/9.3.0/userguide/building_java_projects.html in the Gradle documentation.
  */
 
+dependencies {
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("ch.qos.logback:logback-classic:1.2.10")
+    implementation("ch.qos.logback:logback-core:1.2.10")
+}
+
+
+
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,12 +3,22 @@
  */
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
+        logger.info("Application started");
+        logger.debug("Debugging information");
+        logger.error("An error occurred", new RuntimeException("Test error"));
+
         System.out.println(new App().getGreeting());
     }
 }
